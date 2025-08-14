d109566389caaea460a2eefd34eb3757c8bf2cb7
begumnarmanli <begumnarmanli@gmail.com>
2025-08-15 00:53:29 +0300

    Initial commit: toDoList

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "todo-list-cache-v2";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/toDoList.jpg",
+  "/manifest.json",
+  "/to-do-list 128.png",
+  "/to-do-list 512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Önbelleğe alındı");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Alarm bildirimi için eklenen kod
+self.addEventListener('message', event => {
+    if (event.data.type === 'ALARM_REMINDER') {
+        const { title, body } = event.data;
+        self.registration.showNotification(title, {
+            body: body,
+            icon: 'icon.png' 
+        });
+    }
+});
